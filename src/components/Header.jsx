557619d97b6fde1666bec8f82b8bb96b32cc434c
Alexis REY <alexis.rey159@laposte.net>
2025-06-26 19:03:39 +0200

    Ajout du logo header + nav et import police local, organisation scss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import logoDesktop from "../assets/logo.svg";
+import logoMobile from "../assets/logo-mob.svg";
+import { NavLink } from "react-router-dom";
+
+const liens = [
+    {to: "/", label:"Accueil", end: true},
+    {to:"/a-propos", label:"À propos"}
+];
 
 function Header() {
-    return <div>
-        <nav>Je suis la navigation du header</nav>
-    </div>
+    return <header>
+        <div className="header">
+            <picture>
+                <source srcSet={logoMobile} media="(max-width: 600px)" />
+                <img src={logoDesktop} alt="Logo de l'entreprise Kasa" width={210.32} height={68} />
+            </picture>
+            <nav>
+                {liens.map(({to,label,end}) => {
+                    return <NavLink className="navLink" key={to} to={to} end={end}>{label}</ NavLink>  
+                })}
+            </nav>
+        </div>
+    </header>
 };
 
 export default Header;
