77dc86e19af6f7fafde323d3839b1f6d09f32ee0
Alexis REY <alexis.rey159@laposte.net>
2025-06-27 22:25:48 +0200

    Logique du components Cards ajouter

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logements from "../data/logements.json";
 
 function Cards() {
-
+ return <div className="cards">
+    <ul>
+            {logements.map((logement) => (
+                <li key={logement.id}>
+                     <Link to={`/logement/${logement.id}`}>
+                        {logement.title}
+                    </Link>
+                </li>
+            )
+            )} 
+    </ul>
+ </div>
 }
 
 export default Cards;
