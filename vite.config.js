5fcf532593ea59e03e2b0a68e80643cb49586750
Alexis REY <alexis.rey159@laposte.net>
2025-07-11 17:59:27 +0200

    gestion local vs gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Oc_Projet5_Kasa/'
+  base: process.env.NODE_ENV === 'production' ? '/Oc_Projet5_Kasa/' : '/'
 })
